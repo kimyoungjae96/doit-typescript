03876ed847c558f4442031dc964144d0da7ea339
kimyoungjae <sskim5421@gmail.com>
2020-07-17 22:27:51 +0900

    chapter 02-2

---
+++ ch02-2/src/index.ts
@@ -0,0 +1,8 @@
+import IPerson from "./Person/IPerson"
+import Person , { makePerson } from './Person/Person'
+import Chance from 'chance'
+import * as R from 'ramda'
+
+const chance = new Chance()
+let persons: IPerson[] = R.range(0,2).map((n:number) => new Person(chance.name(),chance.age()))
+console.log(persons)
