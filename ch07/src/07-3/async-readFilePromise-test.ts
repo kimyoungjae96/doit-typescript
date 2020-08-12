6e374e7f7bd973afbe4975e5d412bfaaee7b9be9
kimyoungjae <sskim5421@gmail.com>
2020-08-12 22:52:37 +0900

    ch 07-3

---
+++ ch07/src/07-3/async-readFilePromise-test.ts
@@ -0,0 +1,12 @@
+import {readFilePromise} from '../07-2/readFilePromise'
+
+const readFilesAll = async (filenames: string[]) => {
+    return await Promise.all(
+        filenames.map(filename => readFilePromise(filename))
+    )
+}
+
+readFilesAll(['../../package.json', '../../tsconfig.json']).then(([packageJson,tsconfigJson]: string[]) =>{
+    console.log('<package.json>:', packageJson)
+    console.log('<tsconfig.json>:', tsconfigJson)
+}).catch(err=>console.log('error:',err.message))
